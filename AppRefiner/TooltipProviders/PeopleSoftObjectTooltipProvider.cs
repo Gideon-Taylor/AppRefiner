1ecfc73688e3954ee870948e988b6c164ff21f65
Tim Slater <tim.slater@gideontaylor.com>
2026-02-13 22:41:41 -0600

    Tooltip: Hovering SQL.NAME now shows the formatted SQL in a tooltip.

--- AppRefiner/TooltipProviders/PeopleSoftObjectTooltipProvider.cs
+++ AppRefiner/TooltipProviders/PeopleSoftObjectTooltipProvider.cs
@@ -3,6 +3,9 @@ using AppRefiner.Database.Models;
 using PeopleCodeParser.SelfHosted;
 using PeopleCodeParser.SelfHosted.Nodes;
 using PeopleCodeTypeInfo.Types;
+using SQL.Formatter;
+using SQL.Formatter.Core;
+using SQL.Formatter.Language;
 using System.Text;
 
 namespace AppRefiner.TooltipProviders
@@ -59,7 +62,11 @@ namespace AppRefiner.TooltipProviders
                 ProcessRecordMemberAccess(node, isTargetHover);
             }
 
-            
+            if (IsSQLMemberAccess(node, out var isSQLTargetHover))
+            {
+                ProcessSQLMemberAccess(node, isSQLTargetHover);
+            }
+
         }
 
         /// <summary>
@@ -86,6 +93,24 @@ namespace AppRefiner.TooltipProviders
             // Check if the left side is an identifier with text "RECORD"
             return node.Target is IdentifierNode identifier &&
                    (string.Equals(identifier.Name, "RECORD", StringComparison.OrdinalIgnoreCase) || string.Equals(identifier.Name, "Scroll", StringComparison.OrdinalIgnoreCase));
+        }
+
+        private bool IsSQLMemberAccess(MemberAccessNode node, out bool isTargetHover)
+        {
+            isTargetHover = false;
+            /* Default to type of the member access */
+            var typeInfo = node.GetInferredType();
+
+            /* If they are hovering over the left side, get its type instead */
+            if (node.Target is IdentifierNode && node.Target.SourceSpan.ContainsPosition(CurrentPosition))
+            {
+                isTargetHover = true;
+                typeInfo = node.Target.GetInferredType();
+            }
+
+            // Check if the left side is an identifier with text "RECORD"
+            return node.Target is IdentifierNode identifier &&
+                   string.Equals(identifier.Name, "SQL", StringComparison.OrdinalIgnoreCase);
         }
 
         /// <summary>
@@ -121,5 +146,38 @@ namespace AppRefiner.TooltipProviders
                 RegisterTooltip(node.SourceSpan, $"Record '{recordName}' not found or has no fields.");
             }
         }
+
+        private void ProcessSQLMemberAccess(MemberAccessNode node, bool isTargetHover)
+        {
+            // Make sure we have DataManager available
+            if (DataManager == null || !DataManager.IsConnected)
+                return;
+            // Get the SQL name from the member name
+            string sqlName = (isTargetHover ? node.Target.ToString() : node.MemberName).ToUpperInvariant();
+            // Fetch SQL definition from the database
+            var sqlDef = DataManager.GetSqlDefinition(sqlName);
+            if (sqlDef != null)
+            {
+                StringBuilder tooltipContent = new();
+                FormatConfig formatConfig = FormatConfig.Builder().Indent("  ")
+                    .Uppercase(true)
+                    .LinesBetweenQueries(2)
+                    .MaxColumnLength(80)
+                    .Build();
+                var formatted = SqlFormatter.Of(Dialect.StandardSql)
+                .Extend(cfg => cfg.PlusSpecialWordChars("%").PlusNamedPlaceholderTypes(new string[] { ":" }).PlusOperators(new string[] { "%Concat" }))
+                .Format(sqlDef, formatConfig).Replace("\n", " \n");
+
+                tooltipContent.AppendLine(formatted);
+                // Register the tooltip for the member access node
+                RegisterTooltip(node.SourceSpan, tooltipContent.ToString());
+            }
+            else
+            {
+                // SQL not found
+                RegisterTooltip(node.SourceSpan, $"SQL '{sqlName}' not found.");
+            }
+        }
+
     }
 }
